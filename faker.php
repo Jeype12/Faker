d9c04a242d432edc3ff45848dc2da8ccc756b12b
Jeype12 <202280128@psu.palawan.edu.ph>
2025-03-06 22:47:52 +0800

    Create the faker for office data

--- faker.php
+++ faker.php
@@ -1,19 +1,34 @@
 <?php 
-require ('vendor'/autoload.php);
+require 'vendor/autoload.php';
 
 $host = 'localhost';
 $dbname = 'philippinelocale';
 $user = 'root';
 $password = 'root';
 
-$connection = new mysqli($host, $user, $password, $dbname);
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+$conn = new mysqli($host, $user, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 } else {
     echo "Connected successfully<br>";
 }
 
 $faker = Faker\Factory::create('en_PH');
+
+$stmt = $conn->prepare("INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
+for ($i = 0; $i < 50; $i++) {
+    $name = $faker->company;
+    $contactnum = $faker->phoneNumber;
+    $email = $faker->companyEmail;
+    $address = $faker->address;
+    $city = $faker->city;
+    $country = 'Philippines';
+    $postal = $faker->postcode;
+
+    $stmt->bind_param("sssssss", $name, $contactnum, $email, $address, $city, $country, $postal);
+    $stmt->execute();
+}
+echo "Office table is filled with 50 rows<br>";
 
 
 ?>
